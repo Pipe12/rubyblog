d90ea859bbf94eda970c9df967f12e09864fe80d
Andrés Felipe Burbano <andresfelipeburbano@hotmail.com>
2019-01-22 14:33:37 -0500

    Add funtion create comments to a pst

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,6 @@
 class Comment < ApplicationRecord
-  belongs_to :post
-
   validates :body, presence: true
+  validates :body, length: { minimum: 3 }
+  
+  belongs_to :post
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,5 @@ class Post < ApplicationRecord
   validates :title, :body, presence: true
   validates :body, length: { minimum: 10 }
 
-  has_many :commets
+  has_many :comments
 end
